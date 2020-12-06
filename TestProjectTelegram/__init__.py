c82af8ab71f4fb047d309586155128db889ccbde
Ryize <60941066+Ryize@users.noreply.github.com>
2020-12-06 10:46:20 +0400

    Update __init__.py

--- TestProjectTelegram/__init__.py
+++ TestProjectTelegram/__init__.py
@@ -3,11 +3,10 @@ from datetime import datetime
 import os
 import pymysql
 import time
-import re
 
 
-token = '1485533907:AAHpsaFsw7vVs3m6885VlqA1D1BVG2z4cgI'
-ip_db = '31.31.196.8'
-login_db =  'u1087101_sert'
-password_db = 'Y9s1Z6d4'
-name_db = 'u1087101_sert'
+token = '****'
+ip_db = '****'
+login_db =  '****'
+password_db = '****'
+name_db = '****'
