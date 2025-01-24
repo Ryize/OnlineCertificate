ab644db807458943679b003a7bb24a19b7af3a88
matvejcekasov <chekashovmatvey@gmail.com>
2025-01-24 14:34:31 +0300

    refactor: Исправление порядка импортов

--- TestProjectTelegram/__init__.py
+++ TestProjectTelegram/__init__.py
@@ -1,14 +1,14 @@
-from aiogram import Bot, Dispatcher, executor, types
-from datetime import datetime
 import os
 import pymysql
 import time
 import re
 
+from aiogram import Bot, Dispatcher, executor, types
+from datetime import datetime
 
 token = os.environ.get('token')
 ip_db = os.environ.get('ip_db')
-login_db =  os.environ.get('login_db')
+login_db = os.environ.get('login_db')
 password_db = os.environ.get('password_db')
 name_db = os.environ.get('name_db')
 name_table = os.environ.get('name_table')
